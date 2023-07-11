32ef82d6b9154274c941eb1f72decde322de844d
Administrator2 <Administrator2@DESKTOP-27H3GS8>
2023-07-11 17:38:08 +0200

    Added API endpoint to drop and recreate "data" table and other bug fixes and features

--- index.js
+++ index.js
@@ -13,37 +13,68 @@ const pool = mysql.createPool({
   connectionLimit: 10,
 });
 
-// POST API Endpoint
-app.post('/api/data', async (req, res) => {
-    const { type, data } = req.body;
-    try {
-        const query = 'INSERT INTO data (type, data) VALUES (?, ?)';
-        const [rows, fields] = await pool.execute(query, [type, JSON.stringify(data)]);
-        res.status(201).json({ message: 'Data inserted successfully!' });
-    } catch (error) {
-        console.error('Error inserting data:', error);
-        res.status(500).json({ error: 'Internal server error' });
-    }
+// Authentication Middleware
+const authenticate = (req, res, next) => {
+  const apiKey = req.query.API;
+  const hardcodedKey = '22FFF861F9169F9BCD816662549BCE07A7983C4BC11AC403478A0FDBF632F9A3';
+
+  if (apiKey === hardcodedKey) {
+    next(); // Proceed to the next middleware or route handler
+  } else {
+    res.status(401).json({ error: 'Unauthorized' });
+  }
+};
+
+// API Endpoint to drop and recreate "data" table
+app.post('/api/recreate-table', authenticate, async (req, res) => {
+  try {
+    const dropQuery = 'DROP TABLE IF EXISTS data';
+    await pool.execute(dropQuery);
+
+    const createQuery = `CREATE TABLE data (
+      id INT AUTO_INCREMENT PRIMARY KEY,
+      type VARCHAR(255),
+      data JSON
+    )`;
+    await pool.execute(createQuery);
+
+    res.status(200).json({ message: 'Table recreated successfully!' });
+  } catch (error) {
+    console.error('Error recreating table:', error);
+    res.status(500).json({ error: 'Internal server error' });
+  }
 });
 
-// GET API Endpoint
-app.get('/api/data', async (req, res) => {
-    const type = req.query.type;
-    try {
-        let query = 'SELECT * FROM data';
-        let queryParams = [];
-        if (type) {
-            query += ' WHERE type = ?';
-            queryParams.push(type);
-        }
-        const [rows, fields] = await pool.execute(query, queryParams);
-        res.status(200).json(rows);
-    } catch (error) {
-        console.error('Error retrieving data:', error);
-        res.status(500).json({ error: 'Internal server error' });
-    }
+// POST API Endpoint with authentication
+app.post('/api/data', authenticate, async (req, res) => {
+  const { type, data } = req.body;
+  try {
+    const query = 'INSERT INTO data (type, data) VALUES (?, ?)';
+    const [rows, fields] = await pool.execute(query, [type, JSON.stringify(data)]);
+    res.status(201).json({ message: 'Data inserted successfully!' });
+  } catch (error) {
+    console.error('Error inserting data:', error);
+    res.status(500).json({ error: 'Internal server error' });
+  }
 });
 
+// GET API Endpoint with authentication
+app.get('/api/data', authenticate, async (req, res) => {
+  const type = req.query.type;
+  try {
+    let query = 'SELECT * FROM data';
+    let queryParams = [];
+    if (type) {
+      query += ' WHERE type = ?';
+      queryParams.push(type);
+    }
+    const [rows, fields] = await pool.execute(query, queryParams);
+    res.status(200).json(rows);
+  } catch (error) {
+    console.error('Error retrieving data:', error);
+    res.status(500).json({ error: 'Internal server error' });
+  }
+});
 
 const PORT = 3000;
 app.listen(PORT, () => {
