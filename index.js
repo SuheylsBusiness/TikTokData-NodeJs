59727ae3b3efc98a36fd2707a8904fdab37c79ce
Administrator2 <Administrator2@DESKTOP-27H3GS8>
2023-07-11 18:10:41 +0200

    bug fixes

--- index.js
+++ index.js
@@ -58,63 +58,42 @@ app.post('/api/data', authenticate, async (req, res) => {
   }
 });
 
-// GET API Endpoint with authentication
 app.get('/api/data', authenticate, async (req, res) => {
     const type = req.query.type;
     const period = req.query.period;
     const country = req.query.country;
-    const industry = req.query.industry; // Adding industry parameter
-    const commercial_music = req.query.commercial_music;
-    const new_on_board = req.query.new_on_board;
+    const industry = req.query.industry;
+    const commercial_music = req.query.commercial_music === 'true';
+    const new_on_board = req.query.new_on_board === 'true';
     const rank_type = req.query.rank_type;
-    
+  
     try {
-      let query = 'SELECT * FROM data';
-      let queryParams = [];
-      
-      // Prepare the WHERE clauses and the corresponding query parameters
-      let whereClauses = [];
-      if (type) {
-        whereClauses.push('type = ?');
-        queryParams.push(type);
+      const [rows, fields] = await pool.execute('SELECT * FROM data');
+      let filteredRows = [];
+      for (let row of rows) {
+        row.data = JSON.parse(row.data); // assuming `data` is a stringified JSON
+        let filteredData = row.data.filter(item => {
+          if (type && item.type !== type) return false;
+          if (period && item.period != period) return false;
+          if (country && item.country !== country) return false;
+          if (industry && item.industry !== industry) return false;
+          if (commercial_music && item.commercial_music !== commercial_music) return false;
+          if (new_on_board && item.new_on_board !== new_on_board) return false;
+          if (rank_type && item.rank_type !== rank_type) return false;
+          return true;
+        });
+        if (filteredData.length > 0) {
+          row.data = filteredData;
+          filteredRows.push(row);
+        }
       }
-      if (period) {
-        whereClauses.push('JSON_EXTRACT(data, "$.period") = ?');
-        queryParams.push(period);
-      }
-      if (country) {
-        whereClauses.push('JSON_EXTRACT(data, "$.country") = ?');
-        queryParams.push(country);
-      }
-      if (industry) { // Check and push industry into WHERE clauses
-        whereClauses.push('JSON_EXTRACT(data, "$.industry") = ?');
-        queryParams.push(industry);
-      }
-      if (commercial_music) {
-        whereClauses.push('JSON_EXTRACT(data, "$.commercial_music") = ?');
-        queryParams.push(commercial_music.toString()); // Convert boolean to string
-      }
-      if (new_on_board) {
-        whereClauses.push('JSON_EXTRACT(data, "$.new_on_board") = ?');
-        queryParams.push(new_on_board.toString()); // Convert boolean to string
-      }
-      if (rank_type) {
-        whereClauses.push('JSON_EXTRACT(data, "$.rank_type") = ?');
-        queryParams.push(rank_type);
-      }
-      
-      // If there are any WHERE clauses, append them to the query
-      if (whereClauses.length > 0) {
-        query += ' WHERE ' + whereClauses.join(' AND ');
-      }
-      
-      const [rows, fields] = await pool.execute(query, queryParams);
-      res.status(200).json(rows);
-    } catch (error) {
-      console.error('Error retrieving data:', error);
-      res.status(500).json({ error: 'Internal server error' });
+      res.send(filteredRows);
+    } catch (err) {
+      console.log(err);
+      res.status(500).send('Error occurred while fetching data');
     }
   });
+  
 
 const PORT = 3000;
 app.listen(PORT, () => {
